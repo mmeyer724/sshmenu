09621dbd62036863714fbec2b6e5d8c883682621
Michael Meyer <michael@meyer.io>
2017-11-05 13:00:44 -0500

    Long overdue code cleanup

--- sshmenu/sshmenu.py
+++ sshmenu/sshmenu.py
@@ -13,11 +13,17 @@ from clint.textui import puts, colored
 targets = ''
 config_name = ''
 
+# Time to sleep between transitions
+TRANSITION_DELAY_TIME = 0.5
+
+
 def main():
     global config_name
 
     # Check arguments
-    parser = argparse.ArgumentParser(prog='sshmenu', description='A convenient tool for bookmarking hosts and connecting to them via ssh.')
+    parser = argparse.ArgumentParser(prog='sshmenu',
+                                     description='A convenient tool for bookmarking '
+                                                 'hosts and connecting to them via ssh.')
     parser.add_argument('-c', '--configname', default='config', help='Specify an alternate configuration name.')
     args = parser.parse_args()
 
@@ -49,17 +55,19 @@ def main():
     update_targets()
     display_menu()
 
+
 def get_terminal_height():
     # Return height of terminal as int
-    tput = Popen(["tput", "lines"], stdout=PIPE)
+    tput = Popen(['tput', 'lines'], stdout=PIPE)
     height, stderr = tput.communicate()
 
-    return(int(height))
+    return int(height)
+
 
 def display_help():
     # Clear screen and show the help text
     call(['clear'])
-    puts(colored.cyan('Available commands'))
+    puts(colored.cyan('Available commands (press any key to exit)'))
 
     puts('enter       - Connect to your selection')
     puts('crtl+c | q  - Quit sshmenu')
@@ -73,130 +81,136 @@ def display_help():
     puts('- (minus)   - Move connection down')
 
     # Hang until we get a keypress
-    key = readchar.readkey()
+    readchar.readkey()
+
 
 def connection_create():
     global config_name
 
     call(['clear'])
-    puts(colored.cyan("Create new connection entry\n"))
+    puts(colored.cyan('Create new connection entry'))
+    puts('')
 
-    host = input ('Hostname: ')
+    host = input('Hostname (user@machine): ')
 
-    if host is "":
-        puts("\nNothing done")
-
-        time.sleep(2)
-
+    if host is '':
+        puts('')
+        puts('Nothing done')
+        time.sleep(TRANSITION_DELAY_TIME)
         return
 
-    friendly = input ('Description []: ')
-    command = input ('Command [ssh]: ')
-    options = input ('Options []: ')
+    friendly = input('Description []: ')
+    command = input('Command [ssh]: ')
+    options = input('Command Options []: ')
 
     # Set the defaults if our input was empty
     command = 'ssh' if command == '' else command
     options = [] if options == '' else options.split()
 
-    # Append the new entry to the list and save the config
+    # Append the new target to the config
     config = json.loads(resources.user.read(config_name))
-    config['targets'].append({"command": command, "host": host, "friendly": friendly, "options": options})
-    resources.user.write(config_name, json.dumps(config, indent=4))
+    config['targets'].append({'command': command, 'host': host, 'friendly': friendly, 'options': options})
 
+    # Save the new config
+    resources.user.write(config_name, json.dumps(config, indent=4))
     update_targets()
 
-    puts("\nNew connection added")
-    time.sleep(2)
+    puts('')
+    puts('New connection added')
+    time.sleep(TRANSITION_DELAY_TIME)
+
 
 def connection_edit(selected_target):
     global targets, config_name
 
     call(['clear'])
-    puts(colored.cyan("Editing connection " + targets[selected_target]['host'] + "\n"))
+    puts(colored.cyan('Editing connection %s' % targets[selected_target]['host']))
+    puts('')
 
     target = targets[selected_target]
 
     while True:
-        host = input_prefill ('Hostname: ', target['host'])
+        host = input_prefill('Hostname: ', target['host'])
+        if host is not '':
+            break
 
-        if host is not "": break
-
-    friendly = input_prefill ('Description: ', target['friendly'])
-    command = input_prefill ('Command [ssh]: ', 'ssh' if not target.get('command') else target['command'])
-    options = input_prefill ('Options []: ', ' '.join(target['options']))
+    friendly = input_prefill('Description: ', target['friendly'])
+    command = input_prefill('Command [ssh]: ', 'ssh' if not target.get('command') else target['command'])
+    options = input_prefill('Options []: ', ' '.join(target['options']))
 
     # Set the defaults if our input was empty
     command = 'ssh' if command == '' else command
     options = [] if options == '' else options.split()
 
-    # Delete the old entry and add the edited one in it's place
+    # Delete the old entry insert the edited one in its place
     config = json.loads(resources.user.read(config_name))
-
     del config['targets'][selected_target]
-
-    config['targets'].insert (selected_target, {"command": command, "host": host, "friendly": friendly, "options": options})
+    config['targets'].insert(selected_target,
+                             {'command': command, 'host': host, 'friendly': friendly, 'options': options})
 
     resources.user.write(config_name, json.dumps(config, indent=4))
-
     update_targets()
 
-    puts("\nChanges saved")
-    time.sleep(2)
+    puts('')
+    puts('Changes saved')
+    time.sleep(TRANSITION_DELAY_TIME)
+
 
 def connection_delete(selected_target):
     global targets, config_name
 
     call(['clear'])
-    puts(colored.red('Delete connection entry for ' + targets[selected_target]['host']) + "\n")
+    puts(colored.red('Delete connection entry for %s' % targets[selected_target]['host']))
+    puts('')
 
     while True:
-        response = input ('Are you sure you want to delete this connection [yes|NO]: ').lower()
+        response = input('Are you sure you want to delete this connection [yes|NO]: ').lower()
 
         if response == 'no' or response == 'n' or response == '':
-            puts("\nNothing done")
+            puts('')
+            puts('Nothing done')
             break
 
         if response == 'yes':
             config = json.loads(resources.user.read(config_name))
-
             del config['targets'][selected_target]
 
             resources.user.write(config_name, json.dumps(config, indent=4))
-
             update_targets()
 
-            puts("\nConnection deleted")
+            puts('')
+            puts('Connection deleted')
             break
 
-    time.sleep(2)
+    time.sleep(TRANSITION_DELAY_TIME)
+
 
 def connection_move_up(selected_target):
     global config_name
 
     config = json.loads(resources.user.read(config_name))
-
-    config['targets'].insert (selected_target - 1, config['targets'].pop(selected_target))
+    config['targets'].insert(selected_target - 1, config['targets'].pop(selected_target))
 
     resources.user.write(config_name, json.dumps(config, indent=4))
-
     update_targets()
+
 
 def connection_move_down(selected_target):
     global config_name
 
     config = json.loads(resources.user.read(config_name))
-
-    config['targets'].insert (selected_target + 1, config['targets'].pop(selected_target))
+    config['targets'].insert(selected_target + 1, config['targets'].pop(selected_target))
 
     resources.user.write(config_name, json.dumps(config, indent=4))
-
     update_targets()
+
 
 def update_targets():
     global targets, config_name
 
     config = json.loads(resources.user.read(config_name))
     targets = config['targets']
+
 
 def display_menu():
     global targets
@@ -216,7 +230,9 @@ def display_menu():
     # Get initial terminal height
     terminal_height = get_terminal_height()
 
-    # Set initial visible target range. Subtract 2 because one line is used by the instructions, and one line is always empty at the bottom.
+    # Set initial visible target range.
+    # Subtract 2 because one line is used by the instructions,
+    # and one line is always empty at the bottom.
     visible_target_range = range(terminal_height - 2)
 
     while True:
@@ -237,7 +253,7 @@ def display_menu():
 
         # Generate description and check line length
         for index, target in enumerate(targets):
-            desc = '%2d ' % (index) + target['host'].ljust(longest_host) + ' | ' + target['friendly']
+            desc = '%2d ' % index + target['host'].ljust(longest_host) + ' | ' + target['friendly']
             target['desc'] = desc
             line_length = len(desc)
             if line_length > longest_line:
@@ -275,8 +291,9 @@ def display_menu():
 
         # We need at least one target for our UI to make sense
         if num_targets <= 0:
-            puts("\nWhoops, you don\'t have any connections listed in your config!")
-            puts("Press 'c' to create a new connection")
+            puts('')
+            puts('Whoops, you don\'t have any connections defined in your config!')
+            puts('Press "c" to create a new connection')
 
         # Make sure our selected target is not higher than possible
         # This can happen if you delete the last target
@@ -298,33 +315,25 @@ def display_menu():
         key = readchar.readkey()
 
         if key == readchar.key.UP or key == 'k' and num_targets > 0:
-            # Ensure the new selection would be valid
+            # Ensure the new selection would be valid & reset number input buffer
             if (selected_target - 1) >= 0:
                 selected_target -= 1
-
-            # Empty the number buffer
             number_buffer = []
 
         elif key == readchar.key.DOWN or key == 'j' and num_targets > 0:
-            # Ensure the new selection would be valid
+            # Ensure the new selection would be valid & reset number input buffer
             if (selected_target + 1) <= (num_targets - 1):
                 selected_target += 1
-
-            # Empty the number buffer
             number_buffer = []
 
         elif key == 'g':
-            # Go to top
+            # Go to top & reset number input buffer
             selected_target = 0
-
-            # Empty the number buffer
             number_buffer = []
 
         elif key == 'G':
-            # Go to bottom
+            # Go to bottom & reset number input buffer
             selected_target = num_targets - 1
-
-            # Empty the number buffer
             number_buffer = []
 
         # Check if key is a number
@@ -367,7 +376,7 @@ def display_menu():
                 # After this line, ssh will replace the python process
                 os.execvp(command, args)
             except FileNotFoundError:
-                sys.exit('command not found: {commandname}'.format(commandname=command))
+                sys.exit('Command not found: {commandname}'.format(commandname=command))
 
         elif key == 'h':
             display_help()
@@ -382,24 +391,24 @@ def display_menu():
             connection_edit(selected_target)
 
         elif key == '-' and num_targets > 0:
-            if (selected_target < num_targets):
+            if selected_target < num_targets:
                 connection_move_down(selected_target)
-
                 selected_target += 1
 
         elif key == '+' and num_targets > 0:
-            if (selected_target > 0):
+            if selected_target > 0:
                 connection_move_up(selected_target)
-
                 selected_target -= 1
 
         elif key == readchar.key.CTRL_C or key == 'q':
             exit(0)
 
-def input_prefill (prompt, text):
+
+def input_prefill(prompt, text):
     def hook():
         readline.insert_text(text)
         readline.redisplay()
+
     readline.set_pre_input_hook(hook)
     result = input(prompt)
     readline.set_pre_input_hook()
